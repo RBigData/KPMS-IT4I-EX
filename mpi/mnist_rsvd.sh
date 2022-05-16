4ed27da9b63cb4e3dddef42cdb4cf627895d1ac8
George Ostrouchov <ostrouchovg@ornl.gov>
2022-05-16 19:53:27 +0200

    fix some warnings

--- mpi/mnist_rsvd.sh
+++ mpi/mnist_rsvd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N mnist_rsvd
-#PBS -l select=1:ncpus=128,walltime=00:10:00
+#PBS -l select=1:mpiprocs=64,walltime=00:10:00
 #PBS -q qexp
 #PBS -e mnist_rsvd.e
 #PBS -o mnist_rsvd.o
@@ -10,5 +10,8 @@ pwd
 
 module load R
 echo "loaded R"
+
+## Fix for warnings from libfabric/1.12 bug
+module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0 
 
 time mpirun --map-by ppr:16:node Rscript mnist_rsvd.R
